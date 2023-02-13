416c67a34df0433c9fc71bbba32a70df8616f2c6
nankivel <nankivel@umich.edu>
2023-02-13 15:11:03 -0800

    default to first dataset and merge demographics

--- modules/ingest.py
+++ modules/ingest.py
@@ -13,6 +13,19 @@ def get_cache_data(dataset, local_cache_path):
     local_cache_path = pathlib.Path(local_cache_path).expanduser()
     if read_pickle_or_none(local_cache_path) is None:
         df = get_data(dataset)
+        logging.info(df.shape)
+        df_demog = get_data(
+            "Demographics",
+            fetch_column_list=[
+                "DESYNPUF_ID",
+                "BENE_BIRTH_DT",
+                "BENE_SEX_IDENT_CD",
+                "SP_STATE_CODE",
+                "BENE_COUNTY_CD",
+            ],
+        )
+        df = df.merge(df_demog, how="left", on="DESYNPUF_ID")
+        logging.info(df.shape)
         logging.info(f"Writing data to local cache file {local_cache_path}")
         df.to_pickle(local_cache_path)
     else:
@@ -21,19 +34,26 @@ def get_cache_data(dataset, local_cache_path):
     return df
 
 
-def get_data(dataset):
+def get_data(dataset, fetch_count=1, fetch_column_list=None):
     """
     download, concat and return df of specified dataset
     parameters
-    dataset: ["Inpatient", "Outpatient]
+    dataset: ["Inpatient", "Outpatient","Demographics"]
     """
     base_url = f"https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_2008_to_2010_{dataset}_Claims_Sample_"
+
+    if dataset == "Demographics":
+        base_url = f"https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/Downloads/DE1_0_2008_Beneficiary_Summary_File_Sample_"
+
     df = pd.DataFrame()
-    for i in range(1, 21):
+    for i in range(1, fetch_count + 1):
         logging.info(f"Fetching file {dataset} {i}...")
         url = f"{base_url}{i}.zip"
         file, _ = urllib.request.urlretrieve(url)
-        _df = read_from_zip(file)
+        if fetch_column_list is None:
+            _df = read_from_zip(file)
+        else:
+            _df = read_from_zip(file)[fetch_column_list]
         df = pd.concat([df, _df])
     # convert date fields to datetime type
     for c in [c for c in df.columns if "_DT" in c]:
@@ -63,5 +83,4 @@ def read_pickle_or_none(FILE) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    get_cache_data("Inpatient", "~/Downloads/inpatient.pkl")
     get_cache_data("Outpatient", "~/Downloads/outpatient.pkl")
