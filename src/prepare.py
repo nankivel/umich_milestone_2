dfb3c4fe9a9536b3d936ddbe26fd38639bf933ce
nankivel <nankivel@umich.edu>
2023-02-19 15:53:52 -0800

    lookup column names from params.yaml

--- src/prepare.py
+++ src/prepare.py
@@ -15,6 +15,7 @@ logging.basicConfig(
 )
 
 params = yaml.safe_load(open("params.yaml"))["prepare"]
+params_features = yaml.safe_load(open("params.yaml"))["features"]
 
 if len(sys.argv) != 2:
     logging.info("Arguments error. Usage:\n")
@@ -26,67 +27,14 @@ try:
     df_raw = pd.read_pickle(sys.argv[1])
     logging.info("Complete!")
     df_raw.dropna(subset=["CLM_PMT_AMT"], inplace=True)
-    df_raw.dropna(subset=["CLM_FROM_DT", "CLM_THRU_DT"], inplace=True)
+    df_raw.dropna(subset=params_features["clm_dates"], inplace=True)
     df_raw.dropna(
-        subset=[
-            "ICD9_PRCDR_CD_1",
-            "ICD9_PRCDR_CD_2",
-            "ICD9_PRCDR_CD_3",
-            "ICD9_PRCDR_CD_4",
-            "ICD9_PRCDR_CD_5",
-            "ICD9_PRCDR_CD_6",
-        ],
+        subset=params_features["PRDCR_CD"],
         inplace=True,
         how="all",
     )
     df_raw.dropna(
-        subset=[
-            "HCPCS_CD_1",
-            "HCPCS_CD_2",
-            "HCPCS_CD_3",
-            "HCPCS_CD_4",
-            "HCPCS_CD_5",
-            "HCPCS_CD_6",
-            "HCPCS_CD_7",
-            "HCPCS_CD_8",
-            "HCPCS_CD_9",
-            "HCPCS_CD_10",
-            "HCPCS_CD_11",
-            "HCPCS_CD_12",
-            "HCPCS_CD_13",
-            "HCPCS_CD_14",
-            "HCPCS_CD_15",
-            "HCPCS_CD_16",
-            "HCPCS_CD_17",
-            "HCPCS_CD_18",
-            "HCPCS_CD_19",
-            "HCPCS_CD_20",
-            "HCPCS_CD_21",
-            "HCPCS_CD_22",
-            "HCPCS_CD_23",
-            "HCPCS_CD_24",
-            "HCPCS_CD_25",
-            "HCPCS_CD_26",
-            "HCPCS_CD_27",
-            "HCPCS_CD_28",
-            "HCPCS_CD_29",
-            "HCPCS_CD_30",
-            "HCPCS_CD_31",
-            "HCPCS_CD_32",
-            "HCPCS_CD_33",
-            "HCPCS_CD_34",
-            "HCPCS_CD_35",
-            "HCPCS_CD_36",
-            "HCPCS_CD_37",
-            "HCPCS_CD_38",
-            "HCPCS_CD_39",
-            "HCPCS_CD_40",
-            "HCPCS_CD_41",
-            "HCPCS_CD_42",
-            "HCPCS_CD_43",
-            "HCPCS_CD_44",
-            "HCPCS_CD_45",
-        ],
+        subset=params_features["HCPCS_CD"],
         inplace=True,
         how="all",
     )
