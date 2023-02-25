f24a3129a41cd7e6d3ff9f6af8b0baa20359ce3f
nankivel <nankivel@umich.edu>
2023-02-25 11:15:39 -0800

    add logging

--- src/visualization.py
+++ src/visualization.py
@@ -1,7 +1,15 @@
 import plotly.graph_objects as go
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 def visualize_components3d(c):
+    logging.info("Visualizing components in 3D...")
     x, y, z = c[0], c[1], c[2]
     fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z, mode="markers")])
     fig.update_layout(width=900, height=1000)
